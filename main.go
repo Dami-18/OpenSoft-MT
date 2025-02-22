a1f6e5b9c85363a8062ba681d6a8dc6bd2df426c
Dami-18 <damiyagondha@gmail.com>
2025-02-22 17:52:50 +0530

    more secure hehe

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
-import(
+import (
 	"OpenSoft-MT/database"
+	"OpenSoft-MT/routes"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 func main(){
-	database.ConnectDb("root:<pass>@tcp(localhost:3306)/jwt_demo?parseTime=true") // later change this hardcoded string to non hardcoded
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	database.ConnectDb(connectionString) 
     database.Migrate()
+
+	router := routes.InitRouter()
+	router.Run(":8080")
 }
